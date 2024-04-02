cbac905e77a7bce4314b9bdbff533cf987b5b0a1
丰宇 <867548082@qq.com>
2024-04-02 20:49:37 +0800

    Update realname.js

--- JS/ylss/realname.js
+++ JS/ylss/realname.js
@@ -16,7 +16,7 @@ var body = $response.body
 var json = JSON.parse(body)
 
 if (json["realname"] != ""){
-  body = $response.body.replace(/username":"\S*","realname/g, "username\":\"" + json["realname"] + "\",\"realname")
+  body = $response.body.replace(/username":"\S*","realname/g, "username\":\"" + "(" + json["realname"] + ")" + "\",\"realname")
 }
 
 $done({ body })
