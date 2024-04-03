5fbe6f4686857fa9135cb20e387cdc87de01dab0
丰宇 <867548082@qq.com>
2024-04-03 09:20:32 +0800

    Update realname.js

--- JS/ylss/realname.js
+++ JS/ylss/realname.js
@@ -15,10 +15,10 @@ hostname = *.yaolaoss.cn
 var body = $response.body
 var json = JSON.parse(body)
 
-if (json["realname"] != ""){
-  body = $response.body.replace(/username":"\S*","realname/g, "username\":\"" + json["username"] + "(" + json["realname"] + ")" + "\",\"realname")
-}
-
 body = $response.body.replace(/hyzodiac":"\S*","hyonlychild/g, "hyzodiac\":\"" + json["birthday"].split("-")[1] + "月" + json["birthday"].split("-")[2] + "日" + "\",\"hyonlychild")
+
+if (json["realname"] != ""){
+  body = body.replace(/username":"\S*","realname/g, "username\":\"" + json["username"] + "(" + json["realname"] + ")" + "\",\"realname")
+}
 
 $done({ body })
