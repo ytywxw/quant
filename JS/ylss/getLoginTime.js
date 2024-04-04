871c26fd0ac50b6b44975732d6b8af45d9960302
丰宇 <867548082@qq.com>
2024-04-04 22:23:47 +0800

    update

--- JS/ylss/getLoginTime.js
+++ JS/ylss/getLoginTime.js
@@ -12,15 +12,27 @@ hostname = *.yaolaoss.cn
 
 *************************************/
 
-var body = $response.body
+var body = $$request.body
 var json = JSON.parse(body)
 
-// $task.fetch({
-//   url: "https://qq.com"
-// }).then(response => {
-//   // response.statusCode, response.headers, response.body
-//   console.log($request.data);
-//   $notify("Title", "Subtitle", response.body); // Success!
-// })
-$notify("1", "2", $request.data)
+var appid = json["appid"]
+var token = json["token"]
+var queryid = json["userid"]
+
+const getLoginTime = {
+  url: "https://www.yaolaoss.cn/Appapi/ApiHome/getSearchUser.html",
+  method: "POST",
+  headers: {
+    "Host": "www.yaolaoss.cn",
+    "Content-Length": 142,
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: {
+    "data": {"appid": appid, "token": token, "queryid": userId}
+  }
+}
+
+$task.fetch(getLoginTime).then(response => {
+  $notify("Title", "Subtitle", response.body)
+})
 $done()
