f59f2479e5b78312af452049b245d24eb581f89b
丰宇 <867548082@qq.com>
2024-04-04 22:46:14 +0800

    Update getLoginTime.js

--- JS/ylss/getLoginTime.js
+++ JS/ylss/getLoginTime.js
@@ -12,34 +12,34 @@ hostname = *.yaolaoss.cn
 
 *************************************/
 
-var body = $$request.body
-var json = JSON.parse(body)
+// var body = $$request.body
+// var json = JSON.parse(body)
 
-var appid = json["appid"]
-var token = json["token"]
-var queryid = json["userid"]
+// var appid = json["appid"]
+// var token = json["token"]
+// var queryid = json["userid"]
 
 $notify("0", "0", "0")
 
-const getLoginTime = {
-  url: "https://www.yaolaoss.cn/Appapi/ApiHome/getSearchUser.html",
-  method: "POST",
-  headers: {
-    "Host": "www.yaolaoss.cn",
-    "Content-Length": 142,
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: {
-    "data": {"appid": appid, "token": token, "queryid": queryid}
-  }
-}
+// const getLoginTime = {
+//   url: "https://www.yaolaoss.cn/Appapi/ApiHome/getSearchUser.html",
+//   method: "POST",
+//   headers: {
+//     "Host": "www.yaolaoss.cn",
+//     "Content-Length": 142,
+//     "Content-Type": "application/x-www-form-urlencoded"
+//   },
+//   body: {
+//     "data": {"appid": appid, "token": token, "queryid": queryid}
+//   }
+// }
 
-$task.fetch(getLoginTime).then(response => {
-  $notify("Title", "Subtitle", response.body)
-  $notify("1", "1", "1")
-}, reason => {
-  $notify("Title", "Subtitle", reason.error)
-  $notify("2", "2", "2")
-})
-$notify("3", "3", "3")
+// $task.fetch(getLoginTime).then(response => {
+//   $notify("Title", "Subtitle", response.body)
+//   $notify("1", "1", "1")
+// }, reason => {
+//   $notify("Title", "Subtitle", reason.error)
+//   $notify("2", "2", "2")
+// })
+// $notify("3", "3", "3")
 $done()
