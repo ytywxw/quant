ba9dd3b1f054eecbccafcbbad32d124b17165749
丰宇 <867548082@qq.com>
2024-04-17 21:45:53 +0800

    Update showAllInfo.js

--- JS/ylss/showAllInfo.js
+++ JS/ylss/showAllInfo.js
@@ -18,7 +18,7 @@ var json = JSON.parse(body)
 body = body.replace(/hyzodiac":"[^",]*/g, "hyzodiac\":\"" + json["birthday"].split("-")[1] + "月" + json["birthday"].split("-")[2] + "日 | " + json["hyzodiac"])
 
 if (json["enable"] != "1") {
-  body = json["realname"] == "" ? body.replace(/username":"[^",]*/g, "username\":\"" + "[已注销]" + json["username"]) : body.replace(/username":"[^",]*/g, "username\":\"" + "[已注销]" + json["username"] + "（" + json["realname"] + "）")
+  body = json["realname"] == "" ? body.replace(/username":"[^",]*/g, "username\":\"" + "【已注销】" + json["username"]) : body.replace(/username":"[^",]*/g, "username\":\"" + "【已注销】" + json["username"] + "（" + json["realname"] + "）")
   body = body.replace(/enable":"[^",]*/g, "enable\":\"1")
 } else {
   if (json["realname"] != ""){
